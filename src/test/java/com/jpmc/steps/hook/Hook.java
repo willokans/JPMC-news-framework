7637b154a72792ac7617554b1f3d77f3803be1c9
willokans <Slide2014!>
2022-06-22 09:59:44 +0100

    delete redundant files

--- src/test/java/com/jpmc/steps/hook/Hook.java
+++ src/test/java/com/jpmc/steps/hook/Hook.java
@@ -1,11 +1,20 @@
 package com.jpmc.steps.hook;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hook {
 
-    @Before
-    public void setup(){
+    //can implement a logger instead of system.out.println
 
+    @Before
+    public void setup(Scenario scenario){
+        System.out.println("Test Execution starting for :"+ scenario.getName().toUpperCase());
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        System.out.println("Test Execution ending for :"+ scenario.getName().toUpperCase());
     }
 }
--- src/test/resources/features/GoogleNewsValidation.feature
+++ src/test/resources/features/GoogleNewsValidation.feature
@@ -6,6 +6,6 @@ Feature: Google news validation
     And John needs to check other sources to confirm a news article is valid
     Then John can confirm the validity on the article on "google" search engine
 
-    Scenario: Can not validate broken URL on Google search
-      Given John navigate to a broken Guardian link
-      Then John can't access The Guardian news
+  Scenario: Can not validate broken URL on Google search
+    Given John navigate to a broken Guardian link
+    Then John can't access The Guardian news
